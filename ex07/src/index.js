b38dbead245ff6e1ca416ddc4608f25401df3e6b
Lunjalook <azra.cosickic@gmail.com>
2022-04-18 07:38:19 +0200

    Adding exercise 07 - Set

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,12 +1,8 @@
 // Only change code below this line
 const numberArray = [9, 5, 4, 11, 5, 12, 13, 15, 4, 2, 5];
-let uniqueSet = new Set(
-    numberArray.sort(function (a, b) {
-        return a - b;
-    })
-);
+let uniqueArray = [...new Set(numberArray)];
+uniqueArray.sort(function (a, b) { return a - b });
 
-let uniqueArray = Array.from(uniqueSet);
 console.log(uniqueArray);
 // Only change code above this line
 
